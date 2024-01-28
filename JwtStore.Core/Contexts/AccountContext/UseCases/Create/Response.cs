bd36ca436322dd5b844a1f647f577603e3b624e4
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-01-28 10:57:48 -0300

    specification

---
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Response.cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Notifications;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create;
+
+public class Response: SharedContext.UseCases.Response
+{
+    protected Response()
+    {
+    }
+    
+    public Response(string message, int statusCode, IEnumerable<Notification>? notifications)
+    {
+        Message = message;
+        StatusCode = statusCode;
+        Notifications = notifications;
+    }
+    
+    public Response(string message, ResponseData data)
+    {
+        Message = message;
+        StatusCode = 201;
+        Notifications = null;
+        Data = data;
+    }
+    
+    public ResponseData? Data { get; set; }
+}
+
+public record ResponseData(Guid Id, string Name, string Email);
---
+++ JwtStore.Core/Contexts/SharedContext/UseCases/Response.cs
@@ -0,0 +1,11 @@
+﻿using Flunt.Notifications;
+
+namespace JwtStore.Core.Contexts.SharedContext.UseCases;
+
+public abstract class Response
+{
+    public string Message { get; set; } = string.Empty;
+    public int StatusCode { get; set; } = 400;
+    public bool IsSuccess => StatusCode is >= 200 and <= 299;
+    public IEnumerable<Notification>? Notifications { get; set; }
+}
